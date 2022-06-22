93fb827a1039002e781571641e3b5e3cf7eb6173
Andrew Knowles <87070974+AndrewJKnowles@users.noreply.github.com>
2022-06-22 14:26:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-/*  Program responds to joystick movement and displays real-time, 
-*   mapped coordinates on the LCD and reports them to serial comms port
+/*        Joystick_Coordinate_Test
+*        ========================
 *
-*   Author: Dr Tim Amsdon Feb 2022
-*/
+*        Function:               Displays x- and y-coordinates on the LCD and sends them over the serial comms port.
+*        Circuit Schematic No.:  8        : https://github.com/ELECXJEL2645/Circuit_Schematics
+*        Required Libraries:     Joystick : https://github.com/ELECXJEL2645/Joystick
+*                                N5110    : https://github.com/ELECXJEL2645/N5110
+*
+*        Authored by:            Dr Tim Amsdon
+*        Date:                   02/2022
+*        Collaberators:          
+*        Version:                1.0
+*        Revision Date:          06/2022 
+*        MBED Studio Version:    1.4.1
+*        MBED OS Version:        6.12.0
+*        Board:	                 NUCLEO L476RG  */
 
 #include "mbed.h"
 #include "Joystick.h" 
